c3e4be449cbf1277d78b8e2a0e01d5ab095f3027
Himangshu-101 <himangshu21_ug@ece.nits.ac.in>
2022-10-28 19:44:10 +0530

    Gallery page's responsiveness added

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,73 +1,83 @@
 import React from 'react'
 import "../Gallery/Gallery.css"
 import events from '../../assets/images/events.png'
+import { Link } from 'react-router-dom'
+import {auth} from '../../firebase';
+import Navbar from '../Navbar/Navbar'
 
-export default function Gallery(){
+export default function Gallery(props){
+
+    const{currentUser,setCurrentUser}=props;
     return (
-        <div className='Big_Div'>
-            <h2 className='Glimpses'>Glimpses Of Us</h2>
-            <div className='flex_Container'>
-                <div className='smallDiv'>
-                    <div class="card">
-                        <img class="card-img-top" src={events} alt="Card cap"></img>
+        <>
+        <div className='container'>
+            <Navbar currentUser={currentUser} setCurrentUser={setCurrentUser}/>
+            <div className='Big_Div'>
+                <h2 className='Glimpses'>Glimpses Of Us</h2>
+                <div className='flex_Container'>
+                    <div className='smallDiv'>
+                        <div class="card">
+                            <img class="card-img-top" src={events} alt="Card cap"></img>
+                        </div>
                     </div>
-                </div>
-                <div className='smallDiv'>
-                    <div class="card">
-                        <img class="card-img-top" src={events} alt="Card cap"></img>
+                    <div className='smallDiv'>
+                        <div class="card">
+                            <img class="card-img-top" src={events} alt="Card cap"></img>
+                        </div>
                     </div>
-                </div>
-                <div className='smallDiv'>
-                    <div class="card">
-                        <img class="card-img-top" src={events} alt="Card cap"></img>
+                    <div className='smallDiv'>
+                        <div class="card">
+                            <img class="card-img-top" src={events} alt="Card cap"></img>
+                        </div>
                     </div>
-                </div>
-                <div className='smallDiv'>
-                    <div class="card">
-                        <img class="card-img-top" src={events} alt="Card cap"></img>
+                    <div className='smallDiv'>
+                        <div class="card">
+                            <img class="card-img-top" src={events} alt="Card cap"></img>
+                        </div>
                     </div>
-                </div>
-                <div className='smallDiv'>
-                    <div class="card">
-                        <img class="card-img-top" src={events} alt="Card cap"></img>
+                    <div className='smallDiv'>
+                        <div class="card">
+                            <img class="card-img-top" src={events} alt="Card cap"></img>
+                        </div>
                     </div>
-                </div>
-                <div className='smallDiv'>
-                    <div class="card">
-                        <img class="card-img-top" src={events} alt="Card cap"></img>
+                    <div className='smallDiv'>
+                        <div class="card">
+                            <img class="card-img-top" src={events} alt="Card cap"></img>
+                        </div>
                     </div>
-                </div>
-                <div className='smallDiv'>
-                    <div class="card">
-                        <img class="card-img-top" src={events} alt="Card cap"></img>
+                    <div className='smallDiv'>
+                        <div class="card">
+                            <img class="card-img-top" src={events} alt="Card cap"></img>
+                        </div>
                     </div>
-                </div>
-                <div className='smallDiv'>
-                    <div class="card">
-                        <img class="card-img-top" src={events} alt="Card cap"></img>
+                    <div className='smallDiv'>
+                        <div class="card">
+                            <img class="card-img-top" src={events} alt="Card cap"></img>
+                        </div>
                     </div>
-                </div>
-                <div className='smallDiv'>
-                    <div class="card">
-                        <img class="card-img-top" src={events} alt="Card cap"></img>
+                    <div className='smallDiv'>
+                        <div class="card">
+                            <img class="card-img-top" src={events} alt="Card cap"></img>
+                        </div>
                     </div>
-                </div>
-                <div className='smallDiv'>
-                    <div class="card">
-                        <img class="card-img-top" src={events} alt="Card cap"></img>
+                    <div className='smallDiv'>
+                        <div class="card">
+                            <img class="card-img-top" src={events} alt="Card cap"></img>
+                        </div>
                     </div>
-                </div>
-                <div className='smallDiv'>
-                    <div class="card">
-                        <img class="card-img-top" src={events} alt="Card cap"></img>
+                    <div className='smallDiv'>
+                        <div class="card">
+                            <img class="card-img-top" src={events} alt="Card cap"></img>
+                        </div>
                     </div>
-                </div>
-                <div className='smallDiv'>
-                    <div class="card">
-                        <img class="card-img-top" src={events} alt="Card cap"></img>
+                    <div className='smallDiv'>
+                        <div class="card">
+                            <img class="card-img-top" src={events} alt="Card cap"></img>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        </>
     )
 }
